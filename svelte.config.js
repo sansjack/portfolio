bedb109cf9cb11262cc94a3c76eaf5cc2ed494a3
Jack Sansom <jacksansom7@gmail.com>
2024-07-26 12:25:01 +0800

    removed uneeded mdsvex vite processor

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md', '.svx'],
+	extensions: ['.svelte'],
 	preprocess: [
 		vitePreprocess(),
-        mdsvex({
-            extensions: ['.md', '.svx'],
-        })
+    
     ],
 
 	kit: {
