6f7bdc475bfba6207fffcfc77e87d8a1188f490f
sansjack <contact@jacksansom.com>
2025-07-28 10:16:38 +0100

    feat: job's updated

--- src/lib/icons/jobs/index.ts
+++ src/lib/icons/jobs/index.ts
@@ -1,8 +1,16 @@
-import DunelmIcon from './dunelm.svg?component'
-import ArkivumIcon from './arkivum.svg?component'
-import CanopactIcon from './canopact.webp'
-import CnickIcon from './cnick.webp'
-import NovusIcon from './novus.webp'
-import NexyIcon from './nexymedia.png'
-
-export { DunelmIcon, ArkivumIcon, CanopactIcon, CnickIcon, NovusIcon, NexyIcon }
+import DunelmIcon from "./dunelm.svg?component"
+import ArkivumIcon from "./arkivum.svg?component"
+import CanopactIcon from "./canopact.webp"
+import CnickIcon from "./cnick.webp"
+import NovusIcon from "./novus.webp"
+import NexyIcon from "./nexymedia.png"
+import PersonaLogo from "./persona.svg?component"
+export {
+	DunelmIcon,
+	ArkivumIcon,
+	CanopactIcon,
+	CnickIcon,
+	NovusIcon,
+	NexyIcon,
+	PersonaLogo,
+}
--- src/routes/content.ts
+++ src/routes/content.ts
@@ -1,5 +1,5 @@
-import { MonitroIcon, PortfoloIcon, DailyDebriefIcon } from '$lib/icons'
-import { LayoutTemplate } from 'lucide-svelte'
+import { MonitroIcon, PortfoloIcon, DailyDebriefIcon } from "$lib/icons"
+import { LayoutTemplate } from "lucide-svelte"
 import {
 	DunelmIcon,
 	ArkivumIcon,
@@ -7,92 +7,102 @@ import {
 	CnickIcon,
 	NovusIcon,
 	NexyIcon,
-} from '$lib/icons/jobs'
-import dayjs from 'dayjs'
+	PersonaLogo,
+} from "$lib/icons/jobs"
+import dayjs from "dayjs"
 
 export const projects = [
 	{
-		title: 'boilermate',
+		title: "boilermate",
 		Icon: LayoutTemplate,
 		description:
-			'Share your code boilerplates to help everyone learn and deploy faster!',
-		link: 'https://boilermate.vercel.app',
+			"Share your code boilerplates to help everyone learn and deploy faster!",
+		link: "https://boilermate.vercel.app",
 		date: 1714147200,
 	},
 	{
-		title: 'monitro.dev',
+		title: "monitro.dev",
 		Icon: MonitroIcon,
-		description: 'Simple & Cheap Log Monitoring',
-		link: 'https://monitro.dev',
+		description: "Simple & Cheap Log Monitoring",
+		link: "https://monitro.dev",
 		date: 1722413951,
 	},
 	{
-		title: 'portfolo.app',
+		title: "portfolo.app",
 		Icon: PortfoloIcon,
-		description: 'Create A Portfolio And Add Projects Using Markdown',
-		link: 'https://portfolo.app',
+		description: "Create A Portfolio And Add Projects Using Markdown",
+		link: "https://portfolo.app",
 		date: 1718121600,
 	},
 	{
-		title: 'dailydebrief.io',
+		title: "dailydebrief.io",
 		Icon: DailyDebriefIcon,
-		description: 'Daily Email digest for all your integrations',
-		link: 'https://dailydebrief.io',
+		description: "Daily Email digest for all your integrations",
+		link: "https://dailydebrief.io",
 		date: 1742774482,
 	},
 ]
 
 export const jobExperiences = [
 	{
-		company: 'Dunelm',
+		company: "Dunelm",
 		companyIcon: DunelmIcon,
-		href: 'https://www.dunelm.com',
-		position: 'Site Reliability Engineer',
+		href: "https://www.dunelm.com",
+		position: "Site Reliability Engineer",
 		startDate: 1648731600,
 		endDate: 1704027600,
 	},
 	{
-		company: 'Arkivum',
+		company: "Arkivum",
 		companyIcon: ArkivumIcon,
-		href: 'https://www.arkivum.com',
-		position: 'Junior Site Reliability Engineer',
+		href: "https://www.arkivum.com",
+		position: "Junior Site Reliability Engineer",
 		startDate: 1609419600,
 		endDate: 1646053200,
 	},
 	{
-		company: 'Canopact',
+		company: "Canopact",
 		companyIcon: CanopactIcon,
-		href: 'https://www.canopact.com',
-		position: 'Python Software Engineer',
+		href: "https://www.canopact.com",
+		position: "Python Software Engineer",
 		startDate: 1704027600,
 		endDate: 1709211600,
 		img: true,
 	},
 	{
-		company: 'Cnick',
+		company: "Cnick",
 		companyIcon: CnickIcon,
-		href: 'https://www.cnick.io',
-		position: 'VueJs Mobile Developer',
+		href: "https://www.cnick.io",
+		position: "VueJs Mobile Developer",
 		startDate: 1722434400,
 		endDate: 1725112800,
 		img: true,
 	},
 	{
-		company: 'Novus Studio',
+		company: "Novus Studio",
 		companyIcon: NovusIcon,
-		href: 'https://www.novus.studio',
-		position: 'Svelte Fullstack Developer',
+		href: "https://www.novus.studio",
+		position: "Svelte Fullstack Developer",
 
 		startDate: 1725112800,
 		endDate: 1740784985,
 		img: true,
 	},
 	{
-		company: 'Nexy Media',
+		company: "Nexy Media",
 		companyIcon: NexyIcon,
-		href: 'https://www.nexy.media',
-		position: 'Python / Svelte Fullstack Developer',
+		href: "https://www.nexy.media",
+		position: "Python / Svelte Fullstack Developer",
 		startDate: 1740957785,
+		endDate: 1746087241,
+		img: true,
+	},
+	{
+		company: "Persona AI",
+		companyIcon: PersonaLogo,
+		href: "https://getpersona.xyz",
+		position: "Backend Engineer",
+		startDate: 1746087241,
 		endDate: dayjs().unix(),
 		img: true,
 	},
